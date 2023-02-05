2470460860b542e29c279ea7ab39aba3eb5242e6
abin04mail <abin04mail@gmail.com>
2023-02-05 12:06:03 +0300

    не сохраняет добавленные записи

--- phone_book.py
+++ phone_book.py
@@ -7,35 +7,39 @@ parser.add_argument('-d', '--delete', dest="param2")
 parser.add_argument('-s', '--show', dest="param3", default="all")
 
 args = parser.parse_args()
-print(args)
+# print(args)
 
 # Формат запуска python3.11 /home/serg/'Рабочий стол'/Python71-1/begin/phone_book.py --add "Katya:89123456713"
 if args.param1:
     name, tele = args.param1.split(":")
+    print(name, tele)
     if name in book:
-        book[name] = [book.get(name), int(tele)]
+        book[name] = [int(tele)]
         print("Контакт с именем ", name, " обновлен")
         print(name, ":", book[name])
     else:
         book[name] = int(tele)
         print("Контакт с именем ", name, "добавлен")
         print(name, ":", book[name])
+        print(book)
 
-# Формат запуска python3.11 /home/serg/'Рабочий стол'/Python71-1/begin/phone_book.py --delete "Katya"
-if args.param2:
-    name = args.param2
-    if name in book:
-        del[book][name]
-        print("Контакт с именем ", name, " удален")
-    else:
-        print("Контакта с именем ", name, " в книге нет")
+    # Формат запуска python3.11 /home/serg/'Рабочий стол'/Python71-1/begin/phone_book.py --delete "Katya"
+elif args.param2:
+        name = args.param2
+        if name in book:
+            del[book][name]
+            print("Контакт с именем ", name, " удален")
+        else:
+            print("Контакта с именем ", name, " в книге нет")
 
-# Формат запуска python3.11 /home/serg/'Рабочий стол'/Python71-1/begin/phone_book.py --show "Katya" или --show "all"
-if args.param3:
-    name = args.param3
-    #if name == all:
-    #    print(name, ":", book[name])
-    if name in book:
-        print(book[name])
-    else:
-        print("Контакта с именем ", name, " в книге нет")
+    # Формат запуска python3.11 /home/serg/'Рабочий стол'/Python71-1/begin/phone_book.py --show "Katya" или --show "all"
+elif args.param3:
+        name = args.param3
+        if name == 'all':
+            print(book)
+        elif name in book:
+            print(name, ":", book[name])
+        else:
+            print("Контакта с именем ", name, " в книге нет")
+else:
+     print("Введен неверный аргумент")
