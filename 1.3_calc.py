c9c3c404155185d975ab142847be79e67f311adc
abin04mail <abin04mail@gmail.com>
2023-02-06 21:33:22 +0300

    обработка ошибок ввода

--- 1.3_calc.py
+++ 1.3_calc.py
@@ -1,18 +1,19 @@
-a = int(input('введите первое число '))
-b = int(input('введите второе число '))
-c = input('введите действие ')
-if c == "+":
-    print (a+b)
-elif c == "-":
-    print(a-b)
-elif c == "*":
-    print(a*b)
-elif c == "/":
-    if b != 0:
-        print(a/b)
-    else:
-        print("ошибка деления на 0")
-elif c == "**":
-    print(a**b)
-else:
+try:
+    a = int(input('введите первое число '))
+    b = int(input('введите второе число '))
+    c = input('введите действие ')
+    if c == "+":
+        print (a+b)
+    elif c == "-":
+        print(a-b)
+    elif c == "*":
+        print(a*b)
+    elif c == "/":
+        if b != 0:
+            print(a/b)
+        else:
+            print("ошибка деления на 0")
+    elif c == "**":
+        print(a**b)
+except BaseException:
     print("ошибка ввода")
