d9448d139cabba7608f786e0ad8729db468a7659
abin04mail <abin04mail@gmail.com>
2023-02-06 22:23:23 +0300

    обработка ошибок

--- 1.6_multiplication _table.py
+++ 1.6_multiplication _table.py
@@ -1,6 +1,9 @@
-a = int(input("введите от 1 до 9 - какой столбец таблицы вывести "))
-if a > 0 and a < 10:
-    for i in range(1, 10):
-        print(i, " * ", a, " = ", i * a)
-else:
-    print("неверный введен номер столбца")
+try:
+    a = int(input("введите от 1 до 9 - какой столбец таблицы вывести "))
+    if a > 0 and a < 10:
+        for i in range(1, 10):
+            print(i, " * ", a, " = ", i * a)
+    else:
+        print("неверный введен номер столбца")
+except BaseException:
+    print("введен неверный символ")
