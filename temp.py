24d9d91b4251251d00c19f54f2d071db18e3215e
abin04mail <abin04mail@gmail.com>
2023-02-09 13:14:11 +0300

    ошибка заглавных-прописных букв

--- temp.py
+++ temp.py
@@ -5,24 +5,24 @@ root = Tk()
 col = ['blue','green','red','cyan','magenta','yellow','black','white']
 
 def color(item):
-    draw=col.index(canvas(item, 'fill'))
+    draw=col.index(canvas.itemcget(item, 'fill'))
     if draw <7:
         return draw +1
     return 0
 
 def change_rec():
-    canvas.itemconfig(rec, fill=a[color(rec)])
+    canvas.itemconfig(rec, fill=col[color(rec)])
 
 def change_pol():
-    canvas.itemconfig(pol, fill=a[color(pol)])
+    canvas.itemconfig(pol, fill=col[color(pol)])
 
 def change_ova():
-    canvas.itemconfig(ova, fill=a[color(ova)])
+    canvas.itemconfig(ova, fill=col[color(ova)])
 
 canvas = Canvas(root, width=500, height=700, bg='white')
 canvas.pack()
-rec = canvas.create_rectangle(30,200,470,580,fill='Blue')
-ova = canvas.create_oval(400,10,480,90, fill='Yellow')
+rec = canvas.create_rectangle(30,200,470,580,fill='blue')
+ova = canvas.create_oval(400,10,480,90, fill='yellow')
 pol = canvas.create_polygon(30,200,260,120,470,200, fill='red')
 
 a=Button(text='основание', command=change_rec)
