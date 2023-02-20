497682c0d0ced5c15394faa9858eb6d239025ac0
abin04mail <abin04mail@gmail.com>
2023-02-20 23:52:27 +0300

    v0.0.2

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ print(t)
 for i in t:
     print(i, '->', roman_to_int(i))
 
-a = [4, 58, 1994, 26, 99, 69, 80]
+a = [4, 58, 1994, 26, 99, 69, 80, 2999]
+print(a)
 for i in a:
     print(i, '->', int_to_roman(i))
 
